b03f537f10a8652bdadb676b744501856bc719c1
Junji Ogaki <rosivelto_gato@yahoo.com.br>
2022-10-25 17:46:32 -0300

    implementa controller

---
+++ src/main/java/br/com/petz/clientepet/cliente/application/api/ClienteController.java
@@ -0,0 +1,26 @@
+package br.com.petz.clientepet.cliente.application.api;
+
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@RestController
+@Log4j2
+@RequiredArgsConstructor
+public class ClienteController implements ClienteAPI {
+
+	private final ClienteService clienteService;
+
+	@Override
+	public ClienteResponse postCadastraNovoCliente(@Valid ClienteRequest clienteRequest) {
+		log.info("[inicia] ClienteController - postCadastraNovoCliente");
+		ClienteResponse novoCliente = clienteService.cadastraNovoCliente(clienteRequest);
+		log.info("[finaliza] ClienteController - postCadastraNovoCliente");
+
+		return novoCliente;
+	}
+
+}
