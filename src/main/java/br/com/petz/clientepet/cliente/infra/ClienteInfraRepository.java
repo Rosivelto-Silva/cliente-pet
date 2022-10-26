c8888bbb02e58791dd808a824b37a1cb77f0fd64
Junji Ogaki <rosivelto_gato@yahoo.com.br>
2022-10-26 11:39:06 -0300

    implementa cliente respository. teste OK!

--- src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
+++ src/main/java/br/com/petz/clientepet/cliente/infra/ClienteInfraRepository.java
@@ -12,12 +12,14 @@ import lombok.extern.log4j.Log4j2;
 @RequiredArgsConstructor
 public class ClienteInfraRepository implements ClienteRepository {
 
+	private final ClienteSpringDataJPARepository clienteSpringDataJPARepository;
+
 	@Override
 	public Cliente salva(Cliente cliente) {
 		log.info("[inicia] ClienteInfraRepository - salva");
-		Cliente salvaCliente = clienteSpringDataJPAREpository.save(cliente);
+		Cliente salvaCliente = clienteSpringDataJPARepository.save(cliente);
 		log.info("[finaliza] ClienteInfraRepository - salva");
-		return cliente;
+		return salvaCliente;
 	}
 
 }
